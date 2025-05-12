60ccf7346a77462d5ffd59f52b8d5c3bbc325873
“Marcos” <marcos.wiendl@gmail.com>
2025-05-12 15:09:42 -0300

    feat: add support for gravatar images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,13 @@ const nextConfig: NextConfig = {
     output: 'export',
     images: {
         unoptimized: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'www.gravatar.com',
+                pathname: '/avatar/**',
+            },
+        ],
     },
 };
 
