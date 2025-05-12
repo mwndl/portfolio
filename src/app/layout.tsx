274e20e1d85f690edc6d1d49bd9a798cf48a1702
“Marcos” <marcos.wiendl@gmail.com>
2025-05-12 11:57:54 -0300

    chore: edit navbar import path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
-import Navbar from '@/components/Navbar/Navbar';
+import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
