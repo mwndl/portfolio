f89ba1ad2b00f0dc8bb152aeb6501b7cf6d2e406
“Marcos” <marcos.wiendl@gmail.com>
2025-05-12 12:03:35 -0300

    refactor: comment unused components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import AboutMe from './components/AboutMe';
+/*
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+*/
 
 export default function Home() {
   return (
