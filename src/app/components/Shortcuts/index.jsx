a72c2dbb00d2a01d55745ed35a877beb5db6ec5e
“Marcos” <marcos.wiendl@gmail.com>
2025-05-12 20:46:21 -0300

    feat: add shortcuts component

---
+++ src/app/components/Shortcuts/index.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+import styles from './styles.module.css';
+import { useTranslation } from 'react-i18next';
+
+export default function AboutMe() {
+    const { t } = useTranslation();
+
+    return (
+    <section id="about" className={styles.section}>
+        <div className={styles.container}>
+            <a className={styles.shortcut} href={t('shortcuts.resume_url')} target="_blank">{t('shortcuts.resume')}</a>
+            <a className={styles.shortcut} href="https://www.linkedin.com/in/marcoswiendl/" target="_blank">{t('shortcuts.linkedin')}</a>
+            <a className={styles.shortcut} href="https://github.com/mwndl" target="_blank">{t('shortcuts.github')}</a>
+            <a className={styles.shortcut} href="mailto:contato@marcoswiendl.com" target="_blank">{t('shortcuts.email')}</a>
+        </div>
+    </section>
+    );
+} 
