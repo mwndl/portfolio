ffcd154fd5fcce243c80c07c8ae5b7517cd6967d
“Marcos” <marcos.wiendl@gmail.com>
2025-05-12 15:09:23 -0300

    feat: add blur data for profile image

--- src/app/components/AboutMe/index.tsx
+++ src/app/components/AboutMe/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import styles from './styles.module.css';
 import TypewriterText from '../../../components/TypewriterText/TypewriterText';
 import { useTranslation } from 'react-i18next';
+import { PROFILE_BLUR_DATA } from './constants';
 
 export default function AboutMe() {
     const { t } = useTranslation();
@@ -21,7 +22,9 @@ export default function AboutMe() {
             <div className={styles.imageContainer}>
             <div className={styles.imageWrapper}>
                 <Image
-                src="/images/profile_picture.jpeg"
+                src="https://www.gravatar.com/avatar/d87fbc718cafb7c4a7ce26efd1f227cc"
+                placeholder="blur"
+                blurDataURL={PROFILE_BLUR_DATA}
                 alt="Profile picture"
                 width={500}
                 height={500}
