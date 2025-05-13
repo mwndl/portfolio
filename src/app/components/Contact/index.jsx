c8d177ec09258393fac75d80b807c259feb02922
“Marcos” <marcos.wiendl@gmail.com>
2025-05-13 10:18:48 -0300

    feat: add visible-based animation to contact section

--- src/app/components/Contact/index.jsx
+++ src/app/components/Contact/index.jsx
@@ -4,31 +4,48 @@ import { useState } from 'react';
 import styles from './styles.module.css';
 import { useTranslation } from 'react-i18next';
 import ContactPopup from '../ContactPopup';
+import { useIntersectionObserver } from '@/hooks/useIntersectionObserver';
 
 export default function Contact() {
     const { t } = useTranslation();
     const [isPopupOpen, setIsPopupOpen] = useState(false);
 
+    const [ref, isVisible] = useIntersectionObserver();
+
+    console.log(isVisible)
+
     return (
-    <section id="contact" className={styles.section}>
-        <div className={styles.container}>
-            <div className={styles.textContainer}>
-                <h2 className={styles.title}>{t('contact.title')}</h2>
-                <p className={styles.description}>{t('contact.description1')}</p>
-                <p className={styles.description}>{t('contact.description2')}</p>
+        <section id="contact" className={styles.section} ref={ref}>
+            <div className={`${styles.container} ${isVisible ? styles.fadeInRightToLeft : ''}`}>
+                <div className={styles.textContainer}>
+                    <h2 className={`${styles.title} ${isVisible ? styles.fadeInRightToLeft : ''}`}
+                        style={{ animationDelay: '0.15s' }}
+                    >
+                        {t('contact.title')}
+                    </h2>
+                    <p className={`${styles.description} ${isVisible ? styles.fadeInRightToLeft : ''}`}
+                        style={{ animationDelay: '0.3s' }}>
+                        {t('contact.description1')}
+                    </p>
+                    <p className={`${styles.description} ${isVisible ? styles.fadeInRightToLeft : ''}`}
+                        style={{ animationDelay: '0.45s' }}
+                    >
+                        {t('contact.description2')}
+                    </p>
+                </div>
+                <button 
+                    className={`${styles.contactButton} ${isVisible ? styles.fadeInUp : ''}`}
+                    onClick={() => setIsPopupOpen(true)}
+                    style={{ animationDelay: '0.6s' }}
+                >
+                    {t('contact.buttonText')}
+                </button>
+
+                <ContactPopup 
+                    isOpen={isPopupOpen}
+                    onClose={() => setIsPopupOpen(false)}
+                />
             </div>
-            <button 
-                className={styles.contactButton}
-                onClick={() => setIsPopupOpen(true)}
-            >
-                {t('contact.buttonText')}
-            </button>
-            
-            <ContactPopup 
-                isOpen={isPopupOpen}
-                onClose={() => setIsPopupOpen(false)}
-            />
-        </div>
-    </section>
+        </section>
     );
-} 
+}
