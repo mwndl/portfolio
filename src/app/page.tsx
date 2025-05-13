3d5ba730b596374ab935444c10e4f846f81d6878
“Marcos” <marcos.wiendl@gmail.com>
2025-05-12 22:02:21 -0300

    feat: import skills section component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import AboutMe from './components/AboutMe';
 import Shortcuts from './components/Shortcuts';
-/*
 import Skills from './components/Skills';
+/*
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
@@ -12,8 +12,8 @@ export default function Home() {
     <main className="min-h-screen">
       <AboutMe />
       <Shortcuts />
-      {/*
       <Skills />
+      {/*
       <Experience />
       <Projects />
       <Contact />
