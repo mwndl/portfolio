3cd91f3891978e4ed8d97ada6eb09607ed8f970a
“Marcos” <marcos.wiendl@gmail.com>
2025-05-13 22:00:57 -0300

    feat: add Projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import Shortcuts from './components/Shortcuts';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
-/*
 import Projects from './components/Projects';
-*/
 
 export default function Home() {
   return (
@@ -14,9 +12,7 @@ export default function Home() {
       <Shortcuts />
       <Skills />
       <Experience />
-      {/*
       <Projects />
-      */}
       <Contact />
     </main>
   );
